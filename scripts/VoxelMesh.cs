c4f132a43b6f83abc877ca02aa63075965f4aacc
Leonardo Raele <leonardoraele@gmail.com>
2024-01-04 00:26:48 -0300

    Meshing with multiple materials

--- scripts/VoxelMesh.cs
+++ scripts/VoxelMesh.cs
@@ -6,7 +6,9 @@ namespace Raele.VoxelSandbox;
 [Tool]
 public partial class VoxelMesh : MeshInstance3D {
 	[Export] public MeshingAlgorithm Algorithm = MeshingAlgorithm.SimpleMarchingCubes;
-	[Export] public Material? Material;
+	[Export] public Material? TopFaceMaterial;
+	[Export] public Material? SideFaceMaterial;
+	[Export] public Material? BottomFaceMaterial;
 	[Export] public bool Regenerate = false;
 	[Export] public bool SetSimpleGeometry = false;
 
@@ -63,9 +65,6 @@ public partial class VoxelMesh : MeshInstance3D {
     public override void _Process(double delta)
     {
         base._Process(delta);
-		if (this.MaterialOverride != this.Material) {
-			this.MaterialOverride = this.Material;
-		}
 		if (this.Regenerate) {
 			this.Regenerate = false;
 			this.RegenerateGeometry();
@@ -114,8 +113,13 @@ public partial class VoxelMesh : MeshInstance3D {
 
     private void BuildMesh_Minecraft()
     {
-		SurfaceTool builder = new SurfaceTool();
+		ArrayMesh mesh = new ArrayMesh();
+		SurfaceTool builder;
+
+		// Top face
+		builder = new SurfaceTool();
 		builder.Begin(Mesh.PrimitiveType.Triangles);
+		builder.SetMaterial(this.TopFaceMaterial);
 		for (int z = 0; z < this.Data.Depth; z++) {
 			for (int y = 0; y < this.Data.Height; y++) {
 				for (int x = 0; x < this.Data.Width; x++) {
@@ -138,6 +142,22 @@ public partial class VoxelMesh : MeshInstance3D {
 						builder.SetUV(Vector2.Right);
 						builder.AddVertex(new Vector3(x + 0.5f, y + 0.5f, -z + 0.5f));
 					}
+				}
+			}
+		}
+		builder.GenerateTangents();
+		builder.Commit(mesh);
+
+		// Side faces
+		builder = new SurfaceTool();
+		builder.Begin(Mesh.PrimitiveType.Triangles);
+		builder.SetMaterial(this.SideFaceMaterial);
+		for (int z = 0; z < this.Data.Depth; z++) {
+			for (int y = 0; y < this.Data.Height; y++) {
+				for (int x = 0; x < this.Data.Width; x++) {
+					if (this.Data.Densities[x, y, z] < this.Data.SurfaceLevel) {
+						continue;
+					}
 					// Right face
 					if (x < this.Data.Width - 1 && this.Data.Densities[x + 1, y, z] < this.Data.SurfaceLevel) {
 						builder.SetNormal(Vector3.Right);
@@ -156,7 +176,7 @@ public partial class VoxelMesh : MeshInstance3D {
 					}
 					// Back face (Z-)
 					if (z < this.Data.Depth - 1 && this.Data.Densities[x, y, z + 1] < this.Data.SurfaceLevel) {
-						builder.SetNormal(Vector3.Back);
+						builder.SetNormal(Vector3.Forward);
 						builder.SetUV(Vector2.Down);
 						builder.AddVertex(new Vector3(x - 0.5f, y - 0.5f, -z - 0.5f));
 						builder.SetUV(Vector2.One);
@@ -169,22 +189,6 @@ public partial class VoxelMesh : MeshInstance3D {
 						builder.AddVertex(new Vector3(x + 0.5f, y - 0.5f, -z - 0.5f));
 						builder.SetUV(Vector2.Right);
 						builder.AddVertex(new Vector3(x + 0.5f, y + 0.5f, -z - 0.5f));
-					}
-					// Bottom face
-					if (y > 0 && this.Data.Densities[x, y - 1, z] < this.Data.SurfaceLevel) {
-						builder.SetNormal(Vector3.Down);
-						builder.SetUV(Vector2.Down);
-						builder.AddVertex(new Vector3(x - 0.5f, y - 0.5f, -z - 0.5f));
-						builder.SetUV(Vector2.Zero);
-						builder.AddVertex(new Vector3(x - 0.5f, y - 0.5f, -z + 0.5f));
-						builder.SetUV(Vector2.One);
-						builder.AddVertex(new Vector3(x + 0.5f, y - 0.5f, -z - 0.5f));
-						builder.SetUV(Vector2.One);
-						builder.AddVertex(new Vector3(x + 0.5f, y - 0.5f, -z - 0.5f));
-						builder.SetUV(Vector2.Zero);
-						builder.AddVertex(new Vector3(x - 0.5f, y - 0.5f, -z + 0.5f));
-						builder.SetUV(Vector2.Right);
-						builder.AddVertex(new Vector3(x + 0.5f, y - 0.5f, -z + 0.5f));
 					}
 					// Left face
 					if (x > 0 && this.Data.Densities[x - 1, y, z] < this.Data.SurfaceLevel) {
@@ -204,7 +208,7 @@ public partial class VoxelMesh : MeshInstance3D {
 					}
 					// Front face (Z+)
 					if (z > 0 && this.Data.Densities[x, y, z - 1] < this.Data.SurfaceLevel) {
-						builder.SetNormal(Vector3.Forward);
+						builder.SetNormal(Vector3.Back);
 						builder.SetUV(Vector2.Down);
 						builder.AddVertex(new Vector3(x - 0.5f, y - 0.5f, -z + 0.5f));
 						builder.SetUV(Vector2.Zero);
@@ -222,16 +226,52 @@ public partial class VoxelMesh : MeshInstance3D {
 			}
 		}
 		builder.GenerateTangents();
-		this.Mesh = builder.Commit();
+		builder.Commit(mesh);
+
+		// Bottom face
+		builder = new SurfaceTool();
+		builder.Begin(Mesh.PrimitiveType.Triangles);
+		builder.SetMaterial(this.BottomFaceMaterial);
+		for (int z = 0; z < this.Data.Depth; z++) {
+			for (int y = 0; y < this.Data.Height; y++) {
+				for (int x = 0; x < this.Data.Width; x++) {
+					if (this.Data.Densities[x, y, z] < this.Data.SurfaceLevel) {
+						continue;
+					}
+					// Bottom face
+					if (y > 0 && this.Data.Densities[x, y - 1, z] < this.Data.SurfaceLevel) {
+						builder.SetNormal(Vector3.Down);
+						builder.SetUV(Vector2.Down);
+						builder.AddVertex(new Vector3(x - 0.5f, y - 0.5f, -z - 0.5f));
+						builder.SetUV(Vector2.Zero);
+						builder.AddVertex(new Vector3(x - 0.5f, y - 0.5f, -z + 0.5f));
+						builder.SetUV(Vector2.One);
+						builder.AddVertex(new Vector3(x + 0.5f, y - 0.5f, -z - 0.5f));
+						builder.SetUV(Vector2.One);
+						builder.AddVertex(new Vector3(x + 0.5f, y - 0.5f, -z - 0.5f));
+						builder.SetUV(Vector2.Zero);
+						builder.AddVertex(new Vector3(x - 0.5f, y - 0.5f, -z + 0.5f));
+						builder.SetUV(Vector2.Right);
+						builder.AddVertex(new Vector3(x + 0.5f, y - 0.5f, -z + 0.5f));
+					}
+				}
+			}
+		}
+		builder.GenerateTangents();
+		builder.Commit(mesh);
+
+		this.Mesh = mesh;
     }
 
     private void BuildMesh_SimpleMarchingCubes()
 	{
-		SurfaceTool builder = new SurfaceTool();
+		ArrayMesh mesh = new ArrayMesh();
+		SurfaceTool builder;
+
+		// Top surfaces
+		builder = new SurfaceTool();
 		builder.Begin(Mesh.PrimitiveType.Triangles);
-		if (this.Material != null) {
-			builder.SetMaterial(this.Material);
-		}
+		builder.SetMaterial(this.TopFaceMaterial);
 		for (int z = 0; z < this.Data.Depth - 1; z++) {
 			for (int y = 0; y < this.Data.Height - 1; y++) {
 				for (int x = 0; x < this.Data.Width - 1; x++) {
@@ -247,18 +287,107 @@ public partial class VoxelMesh : MeshInstance3D {
 						this.Data.SurfaceLevel
 					);
 					for (int i = 0; i < 12 && MarchingCubesTable.CaseArrays[mcCaseIndex, i] != -1; i += 3) {
-						builder.SetUV(Vector2.Down);
-						builder.AddVertex(new Vector3(x, y, -z) + MarchingCubesTable.EdgePositionsV[MarchingCubesTable.CaseArrays[mcCaseIndex, i]]);
-						builder.SetUV(Vector2.Right);
-						builder.AddVertex(new Vector3(x, y, -z) + MarchingCubesTable.EdgePositionsV[MarchingCubesTable.CaseArrays[mcCaseIndex, i + 1]]);
-						builder.SetUV(Vector2.Zero);
-						builder.AddVertex(new Vector3(x, y, -z) + MarchingCubesTable.EdgePositionsV[MarchingCubesTable.CaseArrays[mcCaseIndex, i + 2]]);
+						Vector3 v = new Vector3(x, y, -z),
+							v0 = v + MarchingCubesTable.EdgePositionsV[MarchingCubesTable.CaseArrays[mcCaseIndex, i]],
+							v1 = v + MarchingCubesTable.EdgePositionsV[MarchingCubesTable.CaseArrays[mcCaseIndex, i + 1]],
+							v2 = v + MarchingCubesTable.EdgePositionsV[MarchingCubesTable.CaseArrays[mcCaseIndex, i + 2]],
+							cross = (v1 - v0).Cross(v2 - v0);
+						if (cross.Y < 0) {
+							builder.SetUV(Vector2.Down);
+							builder.AddVertex(v0);
+							builder.SetUV(Vector2.Right);
+							builder.AddVertex(v1);
+							builder.SetUV(Vector2.Zero);
+							builder.AddVertex(v2);
+						}
 					}
 				}
 			}
 		}
 		builder.GenerateNormals();
 		builder.GenerateTangents();
-		this.Mesh = builder.Commit();
+		builder.Commit(mesh);
+
+		// Side surfaces
+		builder = new SurfaceTool();
+		builder.Begin(Mesh.PrimitiveType.Triangles);
+		builder.SetMaterial(this.SideFaceMaterial);
+		for (int z = 0; z < this.Data.Depth - 1; z++) {
+			for (int y = 0; y < this.Data.Height - 1; y++) {
+				for (int x = 0; x < this.Data.Width - 1; x++) {
+					int mcCaseIndex = MarchingCubesTable.GetCaseIndex(
+						this.Data.Densities[x, y, z],
+						this.Data.Densities[x + 1, y, z],
+						this.Data.Densities[x, y + 1, z],
+						this.Data.Densities[x + 1, y + 1, z],
+						this.Data.Densities[x, y, z + 1],
+						this.Data.Densities[x + 1, y, z + 1],
+						this.Data.Densities[x, y + 1, z + 1],
+						this.Data.Densities[x + 1, y + 1, z + 1],
+						this.Data.SurfaceLevel
+					);
+					for (int i = 0; i < 12 && MarchingCubesTable.CaseArrays[mcCaseIndex, i] != -1; i += 3) {
+						Vector3 v = new Vector3(x, y, -z),
+							v0 = v + MarchingCubesTable.EdgePositionsV[MarchingCubesTable.CaseArrays[mcCaseIndex, i]],
+							v1 = v + MarchingCubesTable.EdgePositionsV[MarchingCubesTable.CaseArrays[mcCaseIndex, i + 1]],
+							v2 = v + MarchingCubesTable.EdgePositionsV[MarchingCubesTable.CaseArrays[mcCaseIndex, i + 2]],
+							cross = (v1 - v0).Cross(v2 - v0);
+						if (cross.Y == 0) {
+							builder.SetUV(Vector2.Down);
+							builder.AddVertex(v0);
+							builder.SetUV(Vector2.Right);
+							builder.AddVertex(v1);
+							builder.SetUV(Vector2.Zero);
+							builder.AddVertex(v2);
+						}
+					}
+				}
+			}
+		}
+		builder.GenerateNormals();
+		builder.GenerateTangents();
+		builder.Commit(mesh);
+
+		// Bottom surfaces
+		builder = new SurfaceTool();
+		builder.Begin(Mesh.PrimitiveType.Triangles);
+		builder.SetMaterial(this.BottomFaceMaterial);
+		for (int z = 0; z < this.Data.Depth - 1; z++) {
+			for (int y = 0; y < this.Data.Height - 1; y++) {
+				for (int x = 0; x < this.Data.Width - 1; x++) {
+					int mcCaseIndex = MarchingCubesTable.GetCaseIndex(
+						this.Data.Densities[x, y, z],
+						this.Data.Densities[x + 1, y, z],
+						this.Data.Densities[x, y + 1, z],
+						this.Data.Densities[x + 1, y + 1, z],
+						this.Data.Densities[x, y, z + 1],
+						this.Data.Densities[x + 1, y, z + 1],
+						this.Data.Densities[x, y + 1, z + 1],
+						this.Data.Densities[x + 1, y + 1, z + 1],
+						this.Data.SurfaceLevel
+					);
+					for (int i = 0; i < 12 && MarchingCubesTable.CaseArrays[mcCaseIndex, i] != -1; i += 3) {
+						Vector3 v = new Vector3(x, y, -z),
+							v0 = v + MarchingCubesTable.EdgePositionsV[MarchingCubesTable.CaseArrays[mcCaseIndex, i]],
+							v1 = v + MarchingCubesTable.EdgePositionsV[MarchingCubesTable.CaseArrays[mcCaseIndex, i + 1]],
+							v2 = v + MarchingCubesTable.EdgePositionsV[MarchingCubesTable.CaseArrays[mcCaseIndex, i + 2]],
+							cross = (v1 - v0).Cross(v2 - v0);
+						if (cross.Y > 0) {
+							builder.SetUV(Vector2.Down);
+							builder.AddVertex(v0);
+							builder.SetUV(Vector2.Right);
+							builder.AddVertex(v1);
+							builder.SetUV(Vector2.Zero);
+							builder.AddVertex(v2);
+						}
+					}
+				}
+			}
+		}
+		builder.GenerateNormals();
+		builder.GenerateTangents();
+		builder.Commit(mesh);
+
+		this.Mesh = mesh;
 	}
 }
